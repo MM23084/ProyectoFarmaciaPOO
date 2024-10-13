7da3c43c4e2b345cd6842fe36010909ab5ad53fd
MM23084 <mm23084@ues.edu.sv>
2024-10-12 18:29:35 -0600

    Clase COMPRA - Ricardo Mora

---
+++ src/main/java/CLASES/COMPRA.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CLASES;
+
+import CONEXION.DAC;
+import java.sql.ResultSet;
+import java.util.Calendar;
+import java.util.Date;
+import javax.swing.JTextField;
+/**
+ *
+ * @author usuario
+ */
+public class COMPRA extends DAC{
+
+
+    private int cod_compra;
+    private Date fecha;
+    private int cod_provee;
+    private int cod_emp;
+
+    public COMPRA(){
+
+    }
+    public COMPRA(int cod, Date fe){
+        cod_compra =cod;
+        fecha=fe;
+    }
+    public int Cod_emp(){
+        return this.cod_emp;
+    }
+    public void Cod_emp(int c){
+        this.cod_emp = c;
+    }
+    public int cod_provee(){
+        return this.cod_provee;
+    }
+    public void cod_provee(int p){
+        this.cod_provee = p;
+    }
+    public void Codigo(int cod){
+        cod_compra=cod;
+    }
+    public int Codigo(){
+        return cod_compra;
+    }
+
+    public void Fecha(Date f){
+        fecha=f;
+    }
+    public Date Fecha(){
+        return fecha;
+    }
+    public JTextField lblhora;
+    public void fecha(){
+        int i=1;
+        while(i>0){
+        Calendar Cal= Calendar.getInstance();
+
+            String fec= Cal.get(Cal.DATE)+"/"+(Cal.get(Cal.MONTH)+1)+"/"+Cal.get(Cal.YEAR)+" "+Cal.get(Cal.HOUR_OF_DAY)+":"+Cal.get(Cal.MINUTE)+":"+Cal.get(Cal.SECOND);
+            lblhora.setText(fec);
+        }
+    }
+    public ResultSet traerProveedor(String criterio)
+    {
+        String s =
+  "SELECT cod_provee 'Codigo',nombre 'Nombre',nit 'Nit', direccion 'Direccion', telefono 'Telefono', cod_lab 'labotatorio' "
++ "FROM proveedor"
++ "WHERE (nombre like '"+criterio+"%')";
+       return ejecutarSQL(s);
+    }
+    public void Guardar(){
+        prepararSP("{ call insertar_compra(?,?) }");
+        addParametro(1, String.valueOf(cod_provee));
+        addParametro(2, String.valueOf(cod_emp));
+        ejecutarSP();
+    }
+}
